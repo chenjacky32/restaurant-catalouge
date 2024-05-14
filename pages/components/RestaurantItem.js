bca130ff5e7985fa067e649bc0291dd090f87a13
Jacky Chen <chenjacky32@gmail.com>
2024-05-14 15:42:35 +0700

    style component

--- pages/components/RestaurantItem.js
+++ pages/components/RestaurantItem.js
@@ -1,20 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import PropTypes from 'prop-types';
+import Card from "./styled/Card";
+import Container from "./styled/Container";
+import Image from "./styled/Image";
 
 
 export default function RestaurantItem({id,pictureId,name,description}){
-    return(
-        <div className="card">
-            <img src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
-            alt={name} />
-        <div className="container">
-            <h2><Link href={`/detail/${id}`}>{name}</Link></h2>
-            <p>{description}</p>
-            <br />
-        </div>
-        </div>
-    );
+        return(
+            <Card>
+                    <Image
+                        height="250px" 
+                        src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
+                        alt={name}  
+                    />
+                <Container>
+                    <h2><Link href={`/detail/${id}`}>{name}</Link></h2>
+                    <p>{description}</p>
+                    <br />
+                </Container>
+            </Card>
+            );
 }
 
 RestaurantItem.propTypes = {
